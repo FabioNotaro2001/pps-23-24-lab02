895615df5175cfc922f1c9e4097d6508bad72b1b
Fabio Notaro <fabio.notaro2@studio.unibo.it>
2024-03-17 12:30:22 +0100

    Completed production code and testing for task 5

--- src/test/scala/task5/OptionalTest.scala
+++ src/test/scala/task5/OptionalTest.scala
@@ -3,6 +3,7 @@ package task5
 import org.junit.*
 import org.junit.Assert.*
 import Optionals.*
+import task5.Optionals.Optional.filter
 
 class OptionalTest:
   @Test def emptyOptionalShouldBeEmpty(): Unit = {
@@ -37,3 +38,27 @@ class OptionalTest:
     val result = Optional.map(nonEmpty, _ + 1)
     assertEquals(1, Optional.orElse(result, 1))
   }
+
+  @Test def filterWithNonEmptyAndTruePredicateShouldKeepTheValue(): Unit = {
+    val nonEmpty = Optional.Maybe(5)
+    assertEquals(Optional.Maybe(5), filter(nonEmpty)(_ > 2))
+  }
+
+  @Test def filterWithNonEmptyAndFalsePredicateShouldReturnEmpty(): Unit = {
+    val nonEmpty = Optional.Maybe(5)
+    assertEquals(Optional.Empty(), filter(nonEmpty)(_ > 8))
+  }
+
+  @Test def filterWithEmptyShouldReturnEmpty(): Unit = {
+    val empty = Optional.Empty[Int]()
+    assertEquals(Optional.Empty(), filter(empty)(_ > 8))
+  }
+
+  @Test def filterWithStrings(): Unit = {
+    val optionalString = Optional.Maybe("hello")
+    assertEquals(Optional.Maybe("hello"), filter(optionalString)(_.length() > 2))
+  }
+
+  
+
+  
